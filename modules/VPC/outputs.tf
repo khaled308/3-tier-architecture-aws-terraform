f427977eb7f3a4d70e59e7f352a164df773454d3
khaled308 <khaledgaballa4@gmail.com>
2024-12-06 00:21:09 +0200

    output ids of private and public subnets

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -5,3 +5,11 @@ output "public_subnet_names" {
 output "private_subnet_names" {
   value = [for subnet in aws_subnet.private : subnet.tags["Name"]]
 }
+
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "private_subnet_ids" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
