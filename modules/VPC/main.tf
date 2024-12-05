47cccb285ea4208d4dadd94c1ab25faa7bf30f29
khaled308 <khaledgaballa4@gmail.com>
2024-12-05 16:59:41 +0200

    change cidr block data type to list

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -1,60 +1,68 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    tags = {
-        Name = "main"
-    }
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "main"
+  }
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet_cidr
-    availability_zone = var.availability_zone
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "public"
-    }
+  for_each                = toset(var.public_subnet_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = var.availability_zones[each.key % length(var.availability_zones)]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-${each.value}-${var.availability_zones[each.key % length(var.availability_zones)]}"
+  }
+
 }
 
 resource "aws_subnet" "private" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnet_cidr
-    availability_zone = var.availability_zone
-    tags = {
-        Name = "private"
-    }
+  for_each          = toset(var.private_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = each.value
+  availability_zone = var.availability_zones[each.key % length(var.availability_zones)]
+  tags = {
+    Name = "private-${each.value}-${var.availability_zones[each.key % length(var.availability_zones)]}"
+  }
+
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "igw"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "igw"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.main.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
-    tags = {
-        Name = "public"
-    }
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public"
+  }
 }
 
 resource "aws_route_table_association" "public_rta" {
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.public_rt.id
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table" "private_rt" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "private"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private"
+  }
 }
 
 resource "aws_route_table_association" "private_rta" {
-    subnet_id = aws_subnet.private.id
-    route_table_id = aws_route_table.private_rt.id
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
 }
--- modules/VPC/variables.tf
+++ modules/VPC/variables.tf
@@ -3,14 +3,14 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidr" {
-  type = string
+  type = list(string)
 }
 
 variable "private_subnet_cidr" {
-  type = string
+  type = list(string)
 }
 
-variable "availability_zone" {
-    type = string
+variable "availability_zones" {
+  type = list(string)
 }
 
